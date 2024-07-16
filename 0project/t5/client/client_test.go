02d5c77149207f73a33e93217957eb36902a2615
wow <1084716575@qq.com>
2024-07-16 17:30:40 +0800

    1

---
+++ 0project/t5/client/client_test.go
@@ -0,0 +1,38 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+	pb "t5/server/proto"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+
+	file, _ := credentials.NewClientTLSFromFile("E:\\GIT\\go_note\\0project\\t5\\key\\test.pem", "*.tgo.com")
+
+	//连接到 server端，此处禁用安全传输(没有加密和验证)
+	//client, err := grpc.NewClient("127.0.0.1:9090",
+	//	grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//
+	client, err := grpc.NewClient("127.0.0.1:9090",
+		grpc.WithTransportCredentials(file))
+
+	if err != nil {
+		log.Fatalf("未连接： %v", err)
+	}
+	defer client.Close()
+
+	greeterClient := pb.NewGreeterClient(client)
+	hello, err := greeterClient.SayHello(context.Background(), &pb.HelloRequest{Name: "01"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("返回值 %v", hello)
+
+}
---
+++ 0project/t5/server/server_test.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"net"
+	pb "t5/server/proto"
+	"testing"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (c *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{
+		Message: "123",
+	}, nil
+}
+func TestServer(t *testing.T) {
+
+	file, _ := credentials.NewServerTLSFromFile("E:\\GIT\\go_note\\0project\\t5\\key\\test.pem",
+		"E:\\GIT\\go_note\\0project\\t5\\key\\test.key")
+	//开启端口
+	listen, _ := net.Listen("tcp", ":9090")
+	//创建GRPC服务
+	//grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(file))
+	//在grpc中注册服务
+	pb.RegisterGreeterServer(grpcServer, &server{})
+
+	//启动服务
+	err := grpcServer.Serve(listen)
+	if err != nil {
+		fmt.Println("启动服务失败")
+	}
+}
