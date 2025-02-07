5af2fc26d2e4012a8c2d128d7a694ee7f6b3bfc4
shisan1379 <shisan1379@gmail.com>
2025-02-07 09:50:19 +0800

    1

---
+++ 0project/untitled/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"net/http"
+)
+
+func main() {
+	router := gin.New()
+	router.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello")
+	})
+
+	// 为http/2配置参数
+	h2Handler := h2c.NewHandler(router, &http2.Server{})
+	// 配置http服务
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: h2Handler,
+	}
+	// 启动http服务
+	server.ListenAndServe()
+}
